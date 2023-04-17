b5df2b8726fa862772e37dfa39758e39680e2b48
Ylan <hei.nalisoa@gmail.com>
2023-04-17 17:02:51 +0300

    Update AbstractClass1.java

--- src/main/java/springdemo/AbstractClass1.java
+++ src/main/java/springdemo/AbstractClass1.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 public abstract class AbstractClass1 {
   protected final ConcreteDependance1 concreteDependance1;
   protected final ConcreteDependance2 concreteDependance2;
