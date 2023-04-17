05c97441ae5d478b5efb96d338c8078a7be2fcc2
Ylan <hei.nalisoa@gmail.com>
2023-04-17 17:03:20 +0300

    Update ConcreteClass1.java

--- src/main/java/springdemo/ConcreteClass1.java
+++ src/main/java/springdemo/ConcreteClass1.java
@@ -4,8 +4,4 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ConcreteClass1 extends AbstractClass1 {
-
-  public ConcreteClass1(ConcreteDependance1 concreteDependance1, ConcreteDependance2 concreteDependance2) {
-    super(concreteDependance1, concreteDependance2);
-  }
 }
