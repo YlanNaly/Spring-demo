a684665d4c8dc33fb4607c083fc7a2e0da5a21f2
Ylan <hei.nalisoa@gmail.com>
2023-04-17 16:47:56 +0300

    feat: concretize class

--- src/main/java/springdemo/ConcreteClass1.java
+++ src/main/java/springdemo/ConcreteClass1.java
@@ -3,5 +3,9 @@ package springdemo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConcreteClass1 {
+public class ConcreteClass1 extends AbstractClass1 {
+
+  public ConcreteClass1(ConcreteDependance1 concreteDependance1, ConcreteDependance2 concreteDependance2) {
+    super(concreteDependance1, concreteDependance2);
+  }
 }
